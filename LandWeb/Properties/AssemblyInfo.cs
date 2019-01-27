8e469d002d7914236f566ba7956487b857c9f0e3
hudsonchoi <hudsonchoi@gmail.com>
2019-01-26 19:07:07 -0500

    Update again AssemblyInfo

--- LandWeb/Properties/AssemblyInfo.cs
+++ LandWeb/Properties/AssemblyInfo.cs
@@ -32,9 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.1.5")]
-// 1/26/2019 Add Service Place to the cell report; 
-[assembly: AssemblyFileVersion("1.1.5")]
+[assembly: AssemblyVersion("1.1.7")]
+// 1/26/2019 Add Service Place to the cell report;
+// 1.1.6 is for BAChodae
+[assembly: AssemblyFileVersion("1.1.7")]
 
 //[assembly: AssemblyVersion("1.1.5")]
 // 11/10/2018 Bugfix - StatusChange is null; 
